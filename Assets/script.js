5fa75fb1c8712623edde41079ffb158bcf012313
zhihao li <zhihaoli@zhihaos-MBP.fios-router.home>
2022-12-13 23:28:42 -0500

    finalized JavaScript and ReadMe

--- Assets/script.js
+++ Assets/script.js
@@ -1,92 +1,53 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
-
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
-  // placeholder of an empty array to store objects
-  var storage = []
-
-  // select all button with class name 'btn' in html and assign to variable 'saveBtnEl'
+  // select all button with the class name 'btn' in html and assign to variable 'saveBtnEl'
   var saveBtnEl = $(".btn")
 
-  // create a click event with JQuery on() method
+  // create a click event with JQuery on() method on the save button
   saveBtnEl.on("click", function () {
-    // get the id of the parent element of the button element
+    // get the id attribute of the parent element (note: time-block <div> in html) of the button element
     var timeId = this.parentNode.id;
-    // get the textarea element through the children index of the parent element
+    // get the textarea element through the children array index of the parent element (note: time-block <div> in html)
     var descriptionText = this.parentNode.children[1];
-    console.log(descriptionText.value);
-    console.log(timeId);
-    // store key (id of each time-block) and value (text entered in the textarea) in local storage
-    localStorage.setItem(timeId, JSON.stringify(descriptionText.value.trim()));
 
+    // store key (id of each time-block) and value (text entered in the textarea by the user) in localStorage
+    localStorage.setItem(timeId, descriptionText.value.trim());
   })
 
+  // get today's weekday, date and year by Day.js 
+  var today = dayjs();
+  // show today's weekday, date, and year on the <p> element with id "currentDay"
+  $("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+  // get the current time in hour format
+  var timeNow = dayjs().hour();
 
-// get today's weekday, date and year by Day.js API
-var today = dayjs();
-// show today's weekday, date, and year on the <p> element with id "currentDay"
-$("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
+  // use each() method in JQuery to loop over each time-block to extract the time in hour format
+  // by using id attraibute, and change each class based on the hour number comparison
+  $(".time-block").each(function () {
+    // extract the hour number from id attribute with substring() method to remove the first 5 characters: "hour-"
+    var idHour = $(this).attr("id").substring(5);
 
-// get the current time in hour format
-var timeNow = dayjs().hour();
-console.log (timeNow);
+    // reset the "past", "present", and "future" classes if they exist from previous page loading
+    $(this).removeClass("past present future");
 
+    // compare the current time in hour format with the extracted hour number, then add "future", "past",
+    // and "present" classes accordingly.  
+    if (idHour > timeNow) {
+      $(this).addClass("future");
+    } else if (idHour < timeNow) {
+      $(this).addClass("past");
+    } else {
+      $(this).addClass("present");
+    };
 
+    // get user input that was saved in localStorage through the id attribute of each time-block
+    var textInput = localStorage.getItem($(this).attr("id"));
+    // set the value of the textarea element of each time-block with the user input that found in the localStorage
+    $(this.children[1]).val(textInput);
 
-// use each() method in JQuery to loop over each time-block element to extract the hour text
-// in id and change class based on the time-hour number comparison
-$(".time-block").each(function () { 
-  // extract the hour number from id with substring() method to remove the first 5 characters "hour-"
-  var idHour = $(this).attr("id").substring(5);
-  console.log(idHour);
+  });
 
-  // The "past", "present", and "future" classes need to be removed if they exist from previous page loading
-  $(this).removeClass("past present future");
-  
-  // compare the current time in hour format with the extacted hour number and add "future", "past",
-  // and "present" classes accordingly.  
-  if (idHour > timeNow) {
-    $(this).addClass("future");
-  } else if (idHour < timeNow){
-    $(this).addClass("past");
-  } else {
-    $(this).addClass("present");
-  };
-});
-
-
-
-
-
-
-// var idText = "hour-16";
-// var idHour = idText.substring(5);
-// console.log(idHour);
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
-
-
-
-  // TODO: Add code to display the current date in the header of the page.
 });
--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# Work-Day-Scheduler (Placeholder)
+# Work-Day-Scheduler
 
-Hello, my instructor Gabriel Perry gave an extention to this assignment until Jan-4-2023. I will resubmit the assignment as soon as I finish. Please contact my instructor if you have questions. Thank you.
+## Description
+
+This project is to create a simple interactive application that allows a user to save events for each hour of the day from 9 am to 5 pm. The user with a busy schedule can manage time effectively by adding important events to this application.
+
+## Strategies
+
+1. The user will see the time information of current day on the top of the page.
+2. The user will see multiple time-blocks for standard business hours when scrolling down the page.
+3. Each time block is color-coded to indicate whether it is in the past (grey), present (red), or future(green). The color will change dynamically based on the current time.
+4. The user can enter event to each time block.
+5. The event text can be saved to local storage when the user click on save button.
+6. The saved events persist when the user refreshing the page.
+
+## Application URL
+
+https://lzh1990214.github.io/Work-Day-Scheduler/
+
+## Screenshots
+
+Home page displaying current time and allowing user to enter event in each time block:
+
+![Main page with a red 'start" button.](./Assets/S1.png)
+
+Each event text is saved in local storage as value with associated hour as key:
+
+![Main page with a red 'start" button.](./Assets/S2.png)
